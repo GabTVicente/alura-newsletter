41f052713b4250fbeecd1f5cee947312e61cd21a
GabTVicente <gabrieltsujimotovicente@outlook.com>
2022-12-19 18:10:18 -0300

    Finalizando projeto e realizando deploy

--- src/components/ArticleList/Article/Article.jsx
+++ src/components/ArticleList/Article/Article.jsx
@@ -1,18 +1,18 @@
-const Article = ({title, text, tags, image, alt}) =>{
-    return(
-        <div className="flex flex-col items-center p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-sm sm:p-5 gap-2 sm:hover:shadow-gray-600 sm:hover:shadow-lg sm:hover:transition-all sm:hover:drop-shadow-md dark:sm:hover:shadow-black ">
-            <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold">{title}</h3>
-            <div className="w-full flex-row justify-end gap-2 pr-5 hidden sm:flex">
+const Article = ({ title, text, tags, image, alt }) => {
+    return (
+        <div className={styles.containerTitle}>
+            <h3 className={styles.title}>{title}</h3>
+            <div className={styles.containerTag}>
                 {
-                    tags.map((tag) => <span className= "bg-alura-100 dark:bg-dark-100 text-gray-200 font-bold text-xs uppercase px-4 py-1 rounded-full hover:bg-alura-200 hover:scale-110 hover:transition-all" key={tag}>{tag}</span>)
+                    tags.map((tag) => <span className={styles.tag} key={tag}>{tag}</span>)
                 }
             </div>
-            <div className="gap-1 grid">
+            <div className="gap-1 grid line-clamp-6 sm:line-clamp-none">
                 {
-                    text.map((content,index) => <p className='text-alura-200 dark:text-gray-400' key={index}>{content}</p>)
+                    text.map((content, index) => <p className='text-alura-200 dark:text-gray-400' key={index}>{content}</p>)
                 }
             </div>
-            {image && <img className="sm-4 mt-4" src={image}/>}
+            {image && <img className="sm-4 mt-4" src={image} />}
             {image && alt && <span className="sr-only">{alt}</span>}
         </div>
 
@@ -20,3 +20,10 @@ const Article = ({title, text, tags, image, alt}) =>{
 }
 
 export default Article
+
+const styles = {
+    containerTitle: `flex flex-col items-center p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-sm sm:p-5 gap-2 sm:hover:shadow-gray-600 sm:hover:shadow-lg sm:hover:transition-all sm:hover:drop-shadow-md dark:sm:hover:shadow-black `,
+    title: `text-xl text-alura-200 dark:text-gray-200 font-bold`,
+    containerTag: `w-full flex-row justify-end gap-2 pr-5 hidden sm:flex`,
+    tag: `bg-alura-100 dark:bg-dark-100 text-gray-200 font-bold text-xs uppercase px-4 py-1 rounded-full hover:bg-alura-200 hover:scale-110 hover:transition-all hover:delay- hover:bg-gradient-to-r hover:from-alura-200 `,
+}
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,8 +15,12 @@ const Form = ({ onSubmit }) => {
     return <form className="flex flex-col items-center justify-center h-full -mt-20 mx-5 gap-10 outline-none" onSubmit={safeSubmit}>
         <Input required type='text' placeholder="Insira seu nome..." />
         <Input required type='email' placeholder="Insira seu e-mail.." />
-        <button className='hover:animate-pulse hover:shadow-sm hover:shadow-gray-500 hover:dark:shadow-black hover:transition-all w-full max-w-sm bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 dark:text-gray-200 py-1 px-5 uppercase outline-none' type="submit"> Seguir </button>
+        <button className={styles.button} type="submit"> Seguir </button>
     </form>
+}
+
+const styles = {
+    button: `hover:animate-pulse hover:shadow-sm hover:shadow-gray-500 hover:dark:shadow-black hover:transition-all w-full max-w-sm bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 dark:text-gray-200 py-1 px-5 uppercase outline-none hover:-translate-y-1 hover:delay-200`
 }
 
 export default Form
